21f2e27a0d60fe0724a82be7be6035f15ba982e8
Amartya Bose <amartya.bose@gmail.com>
2026-06-04 11:12:19 +0530

    added threaded parallelism

--- src/HEOM/qc-heom.jl
+++ src/HEOM/qc-heom.jl
@@ -4,6 +4,28 @@ using OrdinaryDiffEq
 using ..HEOMStructure
 using ..SpectralDensities, ..Solvents, ..Utilities
 
+function single_propagate(phasespacepoints, solvent, Hamiltonian, sops, nveclist, npluslocs, nminuslocs, γ, c, β, decay, ρ0exp, ntimes, dt, verbose)
+    tspan = (0.0, ntimes * dt)
+    sdim = size(Hamiltonian, 1)
+    ρs = zeros(ComplexF64, ntimes+1, sdim, sdim)
+    workspace = zeros(ComplexF64, sdim, sdim)
+    tmp1 = zeros(ComplexF64, sdim, sdim)
+    Npoints = length(phasespacepoints)
+    update_len = max(1, Npoints ÷ 10)
+    for (j, ps) in enumerate(phasespacepoints)
+        params = HEOMStructure.HEOMParams(Hamiltonian, nothing, nothing, (solvent, ps), sops, nveclist, npluslocs, nminuslocs, γ, c, 0.0, β, decay, workspace, tmp1)
+        prob = ODEProblem{true}(HEOMStructure.scaled_HEOM_RHS!, ρ0exp, tspan, params)
+        sol = solve(prob, Tsit5(), reltol=1e-5, abstol=1e-5, saveat=dt)
+        for t=1:length(sol)
+            @inbounds ρs[t, :, :] .+= sol.u[t][:, :, 1]
+        end
+        if verbose && (j % update_len == 0)
+            @info "Initial condition number $j of $(Npoints) done."
+        end
+    end
+    ρs, Npoints
+end
+
 function propagate(; Hamiltonian::Matrix{ComplexF64}, Jw::AbstractVector{<:SpectralDensities.SpectralDensity}, solvent::Solvents.Solvent, ρ0::Matrix{ComplexF64}, β::Real, dt::Real, ntimes::Int, Lmax::Int, sops::Vector{Matrix{ComplexF64}}, verbose::Bool=false)
     num_modes = 0
     nbaths = length(Jw)
@@ -25,25 +47,23 @@ function propagate(; Hamiltonian::Matrix{ComplexF64}, Jw::AbstractVector{<:Spect
         decay[i] = sum(nvec .* γ)
     end
 
-    tspan = (0.0, ntimes * dt)
     sdim = size(ρ0, 1)
     Nh = length(nveclist)
-    ρs = zeros(ComplexF64, ntimes+1, sdim, sdim)
-    workspace = zeros(ComplexF64, sdim, sdim)
-    tmp1 = zeros(ComplexF64, sdim, sdim)
     ρ0exp = zeros(ComplexF64, sdim, sdim, Nh)
     ρ0exp[:, :, 1] .= ρ0
-    for (j, ps) in enumerate(solvent)
-        verbose && @info "$(j)th iteration"
-        params = HEOMStructure.HEOMParams(Hamiltonian, nothing, nothing, (solvent, ps), sops, nveclist, npluslocs, nminuslocs, γ, c, 0.0, β, decay, workspace, tmp1)
-        prob = ODEProblem{true}(HEOMStructure.scaled_HEOM_RHS!, ρ0exp, tspan, params)
-        sol = solve(prob, Tsit5(), reltol=1e-5, abstol=1e-5, saveat=dt)
-        for t=1:length(sol)
-            @inbounds ρs[t, :, :] .+= sol.u[t][:, :, 1]
-        end
+    chunks = Iterators.partition(solvent, cld(length(solvent), Threads.nthreads()))
+    ρtasks = map(enumerate(chunks)) do (ind, chunk)
+        Threads.@spawn single_propagate(chunk, solvent, Hamiltonian, sops, nveclist, npluslocs, nminuslocs, γ, c, β, decay, copy(ρ0exp), ntimes, dt, verbose && (ind==1))
     end
-    ρs /= solvent.nsamples
-    0:dt:ntimes*dt, ρs
+    results = fetch.(ρtasks)
+    ρs = zero(results[1][1])
+    nsamples = 0
+    for (res, j) in results
+        ρs .+= res
+        nsamples += j
+    end
+
+    0:dt:ntimes*dt, ρs/nsamples
 end
 
 end
